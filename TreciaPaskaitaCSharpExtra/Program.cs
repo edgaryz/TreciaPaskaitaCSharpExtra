42a585ff48a49d4cdd98258ab9537623d8bd20bf
Edgar Sokol <edgar.sokol@gmail.com>
2024-07-19 15:35:21 +0300

    Add project files.

---
+++ TreciaPaskaitaCSharpExtra/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LearningObjects
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArray = { 1, 2, 3, 4};
+
+            Console.WriteLine("///////////////////////////////////////////");
+            Console.WriteLine("TASK 1");
+
+        }
+
+        /*task 1*//*
+        public static void GetArrayElementByIndex(int[] arr, int index)
+        {
+            Console.WriteLine(arr[index]);
+        }*/
+
+    }
+}
